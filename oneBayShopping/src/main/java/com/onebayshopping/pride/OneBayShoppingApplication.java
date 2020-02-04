a3043c894483118269da7d6835eb7c39a01e2e8c
Pride116 <Pridetumisho116@gmail.com>
2020-02-04 13:34:39 +0200

    new start up project run

---
+++ oneBayShopping/src/main/java/com/onebayshopping/pride/OneBayShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.onebayshopping.pride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBayShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneBayShoppingApplication.class, args);
+	}
+
+}
---
+++ oneBayShopping/src/test/java/com/onebayshopping/pride/OneBayShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebayshopping.pride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBayShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
