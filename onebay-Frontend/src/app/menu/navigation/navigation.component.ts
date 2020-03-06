0e3d877a283ce401e61e8e09d7af325cbd3fd357
Pride116 <Pridetumisho116@gmail.com>
2020-03-06 15:44:56 +0200

    updated

--- onebay-Frontend/src/app/menu/navigation/navigation.component.ts
+++ onebay-Frontend/src/app/menu/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/model/book';
+import { BookComponent } from 'src/app/Book/book/book.component';
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  name:any;
   constructor() { }
 
   ngOnInit() {
